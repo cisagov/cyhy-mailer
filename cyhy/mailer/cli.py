7f95592bb9ef248e9a3b943831f775d21bc81ab5
Jeremy Frasier <jeremy.frasier@beta.dhs.gov>
2017-11-30 18:35:14 -0500

    Changing cli.py to be suitable for sending out CYHY reports

--- cyhy/mailer/cli.py
+++ cyhy/mailer/cli.py
@@ -4,33 +4,47 @@
 
 Usage:
   cyhy-mailer [options]
-  cyhy-mailer [--from=EMAIL_ADDRESS] [--to=EMAIL_ADDRESSES] [--cc=EMAIL_ADDRESSES] [--subject=SUBJECT] [--body=BODY_TEXT] [--attachment=FILENAME] [--mail-server=SERVER] [--debug]
+  cyhy-mailer (--report-dir=DIRECTORY) [--mail-server=SERVER] [--mail-port=PORT] [--db-creds-file=FILENAME] [--debug]
   cyhy-mailer (-h | --help)
 
 Options:
   -h --help                   Show this message.
-  -f --from=EMAIL_ADDRESS     The email address that should appear as the
-                              sender of the message.
-  -t --to=EMAIL_ADDRESSES     A comma-delimited list of email addresses to
-                              which the message is being sent.
-  -c --cc=EMAIL_ADDRESSES     A comma-delimited list of email addresses to
-                              which the message is being carbon copied.
-  -s --subject=SUBJECT        The subject of the message.
-  -b --body=BODY_TEXT         The body text of the message.
-  -a --attachment=FILENAME    The PDF filename to attach to the message.
-  -m --mail-server=SERVER     The hostname or IP address of the mail server
-                              that should send the message.
+  -r --report-dir=DIRECTORY   The directory where the PDF reports are located.
+  -m --mail-server=SERVER     The hostname or IP address of the mail server that should send the messages. [default: smtp01.ncats.dhs.gov]
+  -p --mail-port=PORT         The port to use when connecting to the mail
+                              server that should send the messages.
+                              [default: 25]
+  -c --db-creds-file=FILENAME A YAML file containing the CYHY database
+                              credentials.
+                              [default: /run/secrets/database_creds.yml]
   -d --debug                  A Boolean value indicating whether the output
-                              should include error and warning messages or not.
+                              should include debugging messages or not.
 """
 
 import docopt
+import glob
 import logging
 import smtplib
 
+from pymongo import MongoClient
+import yaml
+
 from . import __version__
 from .CyhyMessage import CyhyMessage
-from . import mailer
+
+
+def database_from_config_file(config_filename):
+    with open(config_filename, 'r') as stream:
+        config = yaml.load(stream)
+
+    try:
+        db_uri = config['database']['uri']
+        db_name = config['database']['name']
+    except:
+        logging.error('Incorrect database config file format in {}'.format(config_filename), exc_info=True)
+
+    db_connection = MongoClient(host=db_uri, tz_aware=True)
+    return db_connection[db_name]
 
 
 def main():
@@ -39,21 +53,77 @@ def main():
 
     # Set up logging
     log_level = logging.WARNING
-    if args["--debug"]:
+    if args['--debug']:
         log_level = logging.DEBUG
-    logging.basicConfig(format='%(asctime)-15s %(message)s', level=log_level)
+    logging.basicConfig(format='%(asctime)-15s %(levelname)s %(message)s', level=log_level)
 
-    message = CyhyMessage(['jeremy.frasier@beta.dhs.gov'], '/home/jeremy_frasier/artifacts_2017-11-16/reporting/reports/cyhy-DHS-2017-11-16-tmail-report.pdf', cc_addrs=None)
+    db = database_from_config_file(args['--db-creds-file'])
 
-    server = smtplib.SMTP('smtp01.ncats.dhs.gov', 25)
+    # Set up the connection to the mail server
+    server = smtplib.SMTP(args['--mail-server'], int(args['--mail-port']))
     # server.starttls()
-    mailer.send_message(server, message)
+
+    requests = db.requests.find({'retired': False}, {'_id': True, 'agency.acronym': True, 'agency.contacts.email': True, 'agency.contacts.type': True})
+
+    total_agencies = requests.count()
+    agencies_emailed = 0
+    for request in requests:
+        id = request['_id']
+        acronym = request['agency']['acronym']
+
+        # Drop any contacts that do not have both a type and an email attribute...
+        contacts = [c for c in request['agency']['contacts'] if 'type' in c and 'email' in c]
+        # ...but let's log a warning about them
+        for c in request['agency']['contacts']:
+            if 'type' not in c or 'email' not in c:
+                logging.warn('Agency with ID {} has a contact that is missing an email and/or type attribute!'.format(id))
+
+        distro_emails = [c['email'] for c in contacts if c['type'] == 'DISTRO']
+        technical_emails = [c['email'] for c in contacts if c['type'] == 'TECHNICAL']
+
+        # There should be zero or one distro email, so log a warning if there
+        # are multiple.
+        if len(distro_emails) > 1:
+            logging.warn('More than one DISTRO email address for agency with ID {}'.format(id))
+
+        # Send to the distro email, if it exists.  Otherwise, send to the
+        # technical emails.
+        to_emails = distro_emails
+        if not to_emails:
+            to_emails = technical_emails
+
+        # At this point to_emails should contain at least one email
+        if not to_emails:
+            logging.error('No emails found for ID {}'.format(id))
+            continue
+
+        cyhy_report_glob = '{}/cyhy-{}-*.pdf'.format(args['--report-dir'], id)
+        cyhy_report_filenames = glob.glob(cyhy_report_glob)
+
+        # Exactly one CYHY report should match
+        if len(cyhy_report_filenames) > 1:
+            logging.warn('More than one CYHY report found for agency with ID {}'.format(id))
+            continue
+        elif len(cyhy_report_filenames) == 0:
+            logging.error('No CYHY report found for agency with ID {}'.format(id))
+            continue
+
+        attachment_filename = cyhy_report_filenames[0]
+
+        message = CyhyMessage(to_emails, attachment_filename)
+
+        # mailer.send_message(server, message)
+        agencies_emailed += 1
+
     server.quit()
+
+    # Print out and log some statistics
+    stats_string = 'Out of {} agencies, {} ({}%) were emailed.'.format(total_agencies, agencies_emailed, 100.0 * agencies_emailed / total_agencies)
+    logging.info(stats_string)
+    print(stats_string)
 
     # Stop logging and clean up
     logging.shutdown()
-
-# TO: Pull DISTRO email from cyhy database; if no DISTRO exists, send to every TECHNICAL POC
 
 
 if __name__ == '__main__':
