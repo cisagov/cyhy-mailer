6b9273f0f40ded24cf2c5a4628224011b06d27f5
Jeremy Frasier <jeremy.frasier@trio.dhs.gov>
2019-04-15 10:19:59 -0400

    Get setup.py to work with non-pypi package

--- setup.py
+++ setup.py
@@ -55,7 +55,7 @@ setup(
         "test": ["coveralls", "pre-commit", "pytest", "pytest-cov", "semver"]
     },
     dependency_links=[
-        "http://github.com/cisagov/mongo-db-from-config/tarball/develop#egg=mongo-db-from-config"
+        "http://github.com/cisagov/mongo-db-from-config/tarball/develop#egg=mongo-db-from-config-1.0.0"
     ],
     entry_points={"console_scripts": ["cyhy-mailer = cyhy.mailer.cli:main"]},
 )
