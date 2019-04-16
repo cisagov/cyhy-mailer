b5987102c93bcbcedb4acd4883c45f10f810bf82
Jeremy Frasier <jeremy.frasier@trio.dhs.gov>
2019-04-16 15:52:13 -0400

    Change DHS to CISA in the FOUO blocks

--- tests/test_cybexmessage.py
+++ tests/test_cybexmessage.py
@@ -42,7 +42,7 @@ National Cybersecurity Assessments and Technical Services (NCATS)
 Cybersecurity and Infrastructure Security Agency (CISA)
 ncats@hq.dhs.gov
 
-WARNING: This document is FOR OFFICIAL USE ONLY (FOUO). It contains information that may be exempt from public release under the Freedom of Information Act (5 U.S.G. 552). It is to be controlled, stored, handled, transmitted, distributed, and disposed of in accordance with DHS policy relating to FOUO information and is not to be released to the public or other personnel who do not have a valid 'need-to-know' without prior approval of an authorized DHS official.
+WARNING: This document is FOR OFFICIAL USE ONLY (FOUO). It contains information that may be exempt from public release under the Freedom of Information Act (5 U.S.G. 552). It is to be controlled, stored, handled, transmitted, distributed, and disposed of in accordance with CISA policy relating to FOUO information and is not to be released to the public or other personnel who do not have a valid 'need-to-know' without prior approval of an authorized CISA official.
 """
                 self.assertEqual(part.get_payload(), text_body)
             elif part.get_content_type() == "text/html":
@@ -63,7 +63,7 @@ The NCATS team</p>
 Cybersecurity and Infrastructure Security Agency (CISA)<br>
 <a href="mailto:ncats@hq.dhs.gov">ncats@hq.dhs.gov</a></p>
 
-<p>WARNING: This document is FOR OFFICIAL USE ONLY (FOUO). It contains information that may be exempt from public release under the Freedom of Information Act (5 U.S.G. 552). It is to be controlled, stored, handled, transmitted, distributed, and disposed of in accordance with DHS policy relating to FOUO information and is not to be released to the public or other personnel who do not have a valid 'need-to-know' without prior approval of an authorized DHS official.</p>
+<p>WARNING: This document is FOR OFFICIAL USE ONLY (FOUO). It contains information that may be exempt from public release under the Freedom of Information Act (5 U.S.G. 552). It is to be controlled, stored, handled, transmitted, distributed, and disposed of in accordance with CISA policy relating to FOUO information and is not to be released to the public or other personnel who do not have a valid 'need-to-know' without prior approval of an authorized CISA official.</p>
 </body>
 </html>
 """
@@ -115,7 +115,7 @@ National Cybersecurity Assessments and Technical Services (NCATS)
 Cybersecurity and Infrastructure Security Agency (CISA)
 ncats@hq.dhs.gov
 
-WARNING: This document is FOR OFFICIAL USE ONLY (FOUO). It contains information that may be exempt from public release under the Freedom of Information Act (5 U.S.G. 552). It is to be controlled, stored, handled, transmitted, distributed, and disposed of in accordance with DHS policy relating to FOUO information and is not to be released to the public or other personnel who do not have a valid 'need-to-know' without prior approval of an authorized DHS official.
+WARNING: This document is FOR OFFICIAL USE ONLY (FOUO). It contains information that may be exempt from public release under the Freedom of Information Act (5 U.S.G. 552). It is to be controlled, stored, handled, transmitted, distributed, and disposed of in accordance with CISA policy relating to FOUO information and is not to be released to the public or other personnel who do not have a valid 'need-to-know' without prior approval of an authorized CISA official.
 """
                 self.assertEqual(part.get_payload(), text_body)
             elif part.get_content_type() == "text/html":
@@ -136,7 +136,7 @@ The NCATS team</p>
 Cybersecurity and Infrastructure Security Agency (CISA)<br>
 <a href="mailto:ncats@hq.dhs.gov">ncats@hq.dhs.gov</a></p>
 
-<p>WARNING: This document is FOR OFFICIAL USE ONLY (FOUO). It contains information that may be exempt from public release under the Freedom of Information Act (5 U.S.G. 552). It is to be controlled, stored, handled, transmitted, distributed, and disposed of in accordance with DHS policy relating to FOUO information and is not to be released to the public or other personnel who do not have a valid 'need-to-know' without prior approval of an authorized DHS official.</p>
+<p>WARNING: This document is FOR OFFICIAL USE ONLY (FOUO). It contains information that may be exempt from public release under the Freedom of Information Act (5 U.S.G. 552). It is to be controlled, stored, handled, transmitted, distributed, and disposed of in accordance with CISA policy relating to FOUO information and is not to be released to the public or other personnel who do not have a valid 'need-to-know' without prior approval of an authorized CISA official.</p>
 </body>
 </html>
 """
