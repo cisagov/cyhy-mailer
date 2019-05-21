11ffde9fd1044e025657a476787b592d8500c62a
Dave Redmin <david.redmin@beta.dhs.gov>
2019-05-21 16:41:31 -0400

    Add tests for CyHy notifications

---
+++ tests/test_cyhynotificationmessage.py
@@ -0,0 +1,285 @@
+"""This module contains the tests for the CyhyNotificationMessage class."""
+
+import unittest
+
+from cyhy.mailer.CyhyNotificationMessage import CyhyNotificationMessage
+
+
+class Test(unittest.TestCase):
+    """The tests for the CyhyNotificationMessage class."""
+
+    def test_four_params_single_recipient(self):
+        """Test the 4-parameter version of the constructor."""
+        to = ["recipient@example.com"]
+        pdf = "./tests/data/pdf-sample.pdf"
+        agency_acronym = "CLARKE"
+        report_date = "December 15, 2001"
+
+        message = CyhyNotificationMessage(to, pdf, agency_acronym, report_date)
+
+        self.assertEqual(message["From"], "reports@cyber.dhs.gov")
+        self.assertEqual(
+            message["Subject"],
+            "CLARKE - Cyber Hygiene Alert - New Critical/High Vulnerabilities Detected - December 15, 2001",
+        )
+        self.assertEqual(message["CC"], "ncats@hq.dhs.gov")
+        self.assertEqual(message["To"], "recipient@example.com")
+
+        # Grab the bytes that comprise the attachment
+        bytes = open(pdf, "rb").read()
+
+        # Make sure the correct body and PDF attachments were added
+        for part in message.walk():
+            # multipart/* are just containers
+            if part.get_content_type() == "application/pdf":
+                self.assertEqual(part.get_payload(decode=True), bytes)
+                self.assertEqual(part.get_filename(), "pdf-sample.pdf")
+            elif part.get_content_type() == "text/plain":
+                text_body = """Greetings CLARKE,
+
+Cyber Hygiene scans conducted in the past day indicate potential new critical and/or high vulnerabilities. As part of BOD 19-02, critical findings need to be remediated within 15 days and high findings remediated within 30 days. Details are attached. Same password as before.
+
+If you have any questions, please contact our office.
+
+Cheers,
+The NCATS team
+
+National Cybersecurity Assessments and Technical Services (NCATS)
+Cybersecurity and Infrastructure Security Agency (CISA)
+ncats@hq.dhs.gov
+
+WARNING: This document is FOR OFFICIAL USE ONLY (FOUO). It contains information that may be exempt from public release under the Freedom of Information Act (5 U.S.G. 552). It is to be controlled, stored, handled, transmitted, distributed, and disposed of in accordance with CISA policy relating to FOUO information and is not to be released to the public or other personnel who do not have a valid 'need-to-know' without prior approval of an authorized CISA official.
+"""
+                self.assertEqual(part.get_payload(), text_body)
+            elif part.get_content_type() == "text/html":
+                html_body = """<html>
+<head></head>
+<body>
+<p>Greetings CLARKE,</p>
+
+<p>Cyber Hygiene scans conducted in the past day indicate potential new critical and/or high vulnerabilities. As part of <a href="https://cyber.dhs.gov/bod/19-02/">BOD 19-02</a>, critical findings need to be remediated within 15 days and high findings remediated within 30 days. Details are attached. Same password as before.</p>
+
+<p>If you have any questions, please contact our office.</p>
+
+<p>Cheers,<br>
+The NCATS team</p>
+
+<p>National Cybersecurity Assessments and Technical Services (NCATS)<br>
+Cybersecurity and Infrastructure Security Agency (CISA)<br>
+<a href="mailto:ncats@hq.dhs.gov">ncats@hq.dhs.gov</a></p>
+
+<p>WARNING: This document is FOR OFFICIAL USE ONLY (FOUO). It contains information that may be exempt from public release under the Freedom of Information Act (5 U.S.G. 552). It is to be controlled, stored, handled, transmitted, distributed, and disposed of in accordance with CISA policy relating to FOUO information and is not to be released to the public or other personnel who do not have a valid 'need-to-know' without prior approval of an authorized CISA official.</p>
+</body>
+</html>
+"""
+                self.assertEqual(part.get_payload(), html_body)
+
+    def test_four_params_multiple_recipients(self):
+        """Test the 4-parameter version of the constructor."""
+        to = ["recipient@example.com", "recipient2@example.com"]
+        pdf = "./tests/data/pdf-sample.pdf"
+        agency_acronym = "CLARKE"
+        report_date = "December 15, 2001"
+
+        message = CyhyNotificationMessage(to, pdf, agency_acronym, report_date)
+
+        self.assertEqual(message["From"], "reports@cyber.dhs.gov")
+        self.assertEqual(
+            message["Subject"],
+            "CLARKE - Cyber Hygiene Alert - New Critical/High Vulnerabilities Detected - December 15, 2001",
+        )
+        self.assertEqual(message["CC"], "ncats@hq.dhs.gov")
+        self.assertEqual(message["To"], "recipient@example.com,recipient2@example.com")
+
+        # Grab the bytes that comprise the attachment
+        bytes = open(pdf, "rb").read()
+
+        # Make sure the correct body and PDF attachments were added
+        for part in message.walk():
+            # multipart/* are just containers
+            if part.get_content_type() == "application/pdf":
+                self.assertEqual(part.get_payload(decode=True), bytes)
+                self.assertEqual(part.get_filename(), "pdf-sample.pdf")
+            elif part.get_content_type() == "text/plain":
+                body = """Greetings CLARKE,
+
+Cyber Hygiene scans conducted in the past day indicate potential new critical and/or high vulnerabilities. As part of BOD 19-02, critical findings need to be remediated within 15 days and high findings remediated within 30 days. Details are attached. Same password as before.
+
+If you have any questions, please contact our office.
+
+Cheers,
+The NCATS team
+
+National Cybersecurity Assessments and Technical Services (NCATS)
+Cybersecurity and Infrastructure Security Agency (CISA)
+ncats@hq.dhs.gov
+
+WARNING: This document is FOR OFFICIAL USE ONLY (FOUO). It contains information that may be exempt from public release under the Freedom of Information Act (5 U.S.G. 552). It is to be controlled, stored, handled, transmitted, distributed, and disposed of in accordance with CISA policy relating to FOUO information and is not to be released to the public or other personnel who do not have a valid 'need-to-know' without prior approval of an authorized CISA official.
+"""
+                self.assertEqual(part.get_payload(), body)
+            elif part.get_content_type() == "text/html":
+                html_body = """<html>
+<head></head>
+<body>
+<p>Greetings CLARKE,</p>
+
+<p>Cyber Hygiene scans conducted in the past day indicate potential new critical and/or high vulnerabilities. As part of <a href="https://cyber.dhs.gov/bod/19-02/">BOD 19-02</a>, critical findings need to be remediated within 15 days and high findings remediated within 30 days. Details are attached. Same password as before.</p>
+
+<p>If you have any questions, please contact our office.</p>
+
+<p>Cheers,<br>
+The NCATS team</p>
+
+<p>National Cybersecurity Assessments and Technical Services (NCATS)<br>
+Cybersecurity and Infrastructure Security Agency (CISA)<br>
+<a href="mailto:ncats@hq.dhs.gov">ncats@hq.dhs.gov</a></p>
+
+<p>WARNING: This document is FOR OFFICIAL USE ONLY (FOUO). It contains information that may be exempt from public release under the Freedom of Information Act (5 U.S.G. 552). It is to be controlled, stored, handled, transmitted, distributed, and disposed of in accordance with CISA policy relating to FOUO information and is not to be released to the public or other personnel who do not have a valid 'need-to-know' without prior approval of an authorized CISA official.</p>
+</body>
+</html>
+"""
+                self.assertEqual(part.get_payload(), html_body)
+
+    def test_six_params_single_cc(self):
+        """Test the 6-parameter version of the constructor."""
+        to = ["recipient@example.com", "recipient2@example.com"]
+        pdf = "./tests/data/pdf-sample.pdf"
+        fm = "sender@example.com"
+        cc = ["cc@example.com"]
+        agency_acronym = "CLARKE"
+        report_date = "December 15, 2001"
+
+        message = CyhyNotificationMessage(
+            to, pdf, agency_acronym, report_date, from_addr=fm, cc_addrs=cc
+        )
+
+        self.assertEqual(message["From"], fm)
+        self.assertEqual(
+            message["Subject"],
+            "CLARKE - Cyber Hygiene Alert - New Critical/High Vulnerabilities Detected - December 15, 2001",
+        )
+        self.assertEqual(message["CC"], "cc@example.com")
+        self.assertEqual(message["To"], "recipient@example.com,recipient2@example.com")
+
+        # Grab the bytes that comprise the attachment
+        bytes = open(pdf, "rb").read()
+
+        # Make sure the correct body and PDF attachments were added
+        for part in message.walk():
+            # multipart/* are just containers
+            if part.get_content_type() == "application/pdf":
+                self.assertEqual(part.get_payload(decode=True), bytes)
+                self.assertEqual(part.get_filename(), "pdf-sample.pdf")
+            elif part.get_content_type() == "text/plain":
+                body = """Greetings CLARKE,
+
+Cyber Hygiene scans conducted in the past day indicate potential new critical and/or high vulnerabilities. As part of BOD 19-02, critical findings need to be remediated within 15 days and high findings remediated within 30 days. Details are attached. Same password as before.
+
+If you have any questions, please contact our office.
+
+Cheers,
+The NCATS team
+
+National Cybersecurity Assessments and Technical Services (NCATS)
+Cybersecurity and Infrastructure Security Agency (CISA)
+ncats@hq.dhs.gov
+
+WARNING: This document is FOR OFFICIAL USE ONLY (FOUO). It contains information that may be exempt from public release under the Freedom of Information Act (5 U.S.G. 552). It is to be controlled, stored, handled, transmitted, distributed, and disposed of in accordance with CISA policy relating to FOUO information and is not to be released to the public or other personnel who do not have a valid 'need-to-know' without prior approval of an authorized CISA official.
+"""
+                self.assertEqual(part.get_payload(), body)
+            elif part.get_content_type() == "text/html":
+                html_body = """<html>
+<head></head>
+<body>
+<p>Greetings CLARKE,</p>
+
+<p>Cyber Hygiene scans conducted in the past day indicate potential new critical and/or high vulnerabilities. As part of <a href="https://cyber.dhs.gov/bod/19-02/">BOD 19-02</a>, critical findings need to be remediated within 15 days and high findings remediated within 30 days. Details are attached. Same password as before.</p>
+
+<p>If you have any questions, please contact our office.</p>
+
+<p>Cheers,<br>
+The NCATS team</p>
+
+<p>National Cybersecurity Assessments and Technical Services (NCATS)<br>
+Cybersecurity and Infrastructure Security Agency (CISA)<br>
+<a href="mailto:ncats@hq.dhs.gov">ncats@hq.dhs.gov</a></p>
+
+<p>WARNING: This document is FOR OFFICIAL USE ONLY (FOUO). It contains information that may be exempt from public release under the Freedom of Information Act (5 U.S.G. 552). It is to be controlled, stored, handled, transmitted, distributed, and disposed of in accordance with CISA policy relating to FOUO information and is not to be released to the public or other personnel who do not have a valid 'need-to-know' without prior approval of an authorized CISA official.</p>
+</body>
+</html>
+"""
+                self.assertEqual(part.get_payload(), html_body)
+
+    def test_six_params_multiple_cc(self):
+        """Test the 6-parameter version of the constructor."""
+        to = ["recipient@example.com", "recipient2@example.com"]
+        pdf = "./tests/data/pdf-sample.pdf"
+        fm = "sender@example.com"
+        cc = ["cc@example.com", "cc2@example.com"]
+        agency_acronym = "CLARKE"
+        report_date = "December 15, 2001"
+
+        message = CyhyNotificationMessage(
+            to, pdf, agency_acronym, report_date, from_addr=fm, cc_addrs=cc
+        )
+
+        self.assertEqual(message["From"], fm)
+        self.assertEqual(
+            message["Subject"],
+            "CLARKE - Cyber Hygiene Alert - New Critical/High Vulnerabilities Detected - December 15, 2001",
+        )
+        self.assertEqual(message["CC"], "cc@example.com,cc2@example.com")
+        self.assertEqual(message["To"], "recipient@example.com,recipient2@example.com")
+
+        # Grab the bytes that comprise the attachment
+        bytes = open(pdf, "rb").read()
+
+        # Make sure the correct body and PDF attachments were added
+        for part in message.walk():
+            # multipart/* are just containers
+            if part.get_content_type() == "application/pdf":
+                self.assertEqual(part.get_payload(decode=True), bytes)
+                self.assertEqual(part.get_filename(), "pdf-sample.pdf")
+            elif part.get_content_type() == "text/plain":
+                body = """Greetings CLARKE,
+
+Cyber Hygiene scans conducted in the past day indicate potential new critical and/or high vulnerabilities. As part of BOD 19-02, critical findings need to be remediated within 15 days and high findings remediated within 30 days. Details are attached. Same password as before.
+
+If you have any questions, please contact our office.
+
+Cheers,
+The NCATS team
+
+National Cybersecurity Assessments and Technical Services (NCATS)
+Cybersecurity and Infrastructure Security Agency (CISA)
+ncats@hq.dhs.gov
+
+WARNING: This document is FOR OFFICIAL USE ONLY (FOUO). It contains information that may be exempt from public release under the Freedom of Information Act (5 U.S.G. 552). It is to be controlled, stored, handled, transmitted, distributed, and disposed of in accordance with CISA policy relating to FOUO information and is not to be released to the public or other personnel who do not have a valid 'need-to-know' without prior approval of an authorized CISA official.
+"""
+                self.assertEqual(part.get_payload(), body)
+            elif part.get_content_type() == "text/html":
+                html_body = """<html>
+<head></head>
+<body>
+<p>Greetings CLARKE,</p>
+
+<p>Cyber Hygiene scans conducted in the past day indicate potential new critical and/or high vulnerabilities. As part of <a href="https://cyber.dhs.gov/bod/19-02/">BOD 19-02</a>, critical findings need to be remediated within 15 days and high findings remediated within 30 days. Details are attached. Same password as before.</p>
+
+<p>If you have any questions, please contact our office.</p>
+
+<p>Cheers,<br>
+The NCATS team</p>
+
+<p>National Cybersecurity Assessments and Technical Services (NCATS)<br>
+Cybersecurity and Infrastructure Security Agency (CISA)<br>
+<a href="mailto:ncats@hq.dhs.gov">ncats@hq.dhs.gov</a></p>
+
+<p>WARNING: This document is FOR OFFICIAL USE ONLY (FOUO). It contains information that may be exempt from public release under the Freedom of Information Act (5 U.S.G. 552). It is to be controlled, stored, handled, transmitted, distributed, and disposed of in accordance with CISA policy relating to FOUO information and is not to be released to the public or other personnel who do not have a valid 'need-to-know' without prior approval of an authorized CISA official.</p>
+</body>
+</html>
+"""
+                self.assertEqual(part.get_payload(), html_body)
+
+
+if __name__ == "__main__":
+    unittest.main()
