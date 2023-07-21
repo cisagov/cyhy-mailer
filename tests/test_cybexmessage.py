23237e049589bccde16645f17d3ec9e8882ffbec
David Redmin <david.redmin@gwe.cisa.dhs.gov>
2023-07-21 15:08:53 -0400

    Update CybEx tests to expect 4 CSVs instead of 2

--- tests/test_cybexmessage.py
+++ tests/test_cybexmessage.py
@@ -16,7 +16,7 @@ class Test(unittest.TestCase):
         csv = "./tests/data/csv-sample.csv"
         report_date = "December 15, 2001"
 
-        message = CybexMessage(pdf, csv, csv, report_date)
+        message = CybexMessage(pdf, csv, csv, csv, csv, report_date)
 
         self.assertEqual(message["From"], "reports@cyber.dhs.gov")
         self.assertEqual(
@@ -91,6 +91,8 @@ Cybersecurity and Infrastructure Security Agency<br>
 
         message = CybexMessage(
             pdf,
+            csv,
+            csv,
             csv,
             csv,
             report_date,
